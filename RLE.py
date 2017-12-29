85775b995014eca31230f2988a3e3c8a770396b5
Tobias Valbjørn <tvalbjoern@gmail.com>
2017-12-29 12:50:10 +0100

    modified the non-condensed version

--- RLE.py
+++ RLE.py
@@ -20,35 +20,29 @@ def encode(mess):
 
 
 def decode(mess):
-	return ''.join([n*c for n,c in mess])
+	#return ''.join([n*c for n,c in mess])
 	
 	#my first approach is down below. The above is condensed.
 	# #in case of no input. Don't do anything
-	# if not mess : return ''
+	if not mess : return ''
 	
-	# res = []
+	res = []
 	
-	# #how many elements are there in the list?
-	# end=len(mess)
-	
-	# #use the tuple to traverse through the list of tuples in mess, 
-	# #until the end of the list is reached.
-	# for t in mess:
+	for t in mess:
 		
-		# #the number of times the character occurs 
-		# #is in the first spot in the tuple. 
-		# n=t[0]
+		#The number of times the character occurs 
+		n=t[0]
 		
-		# #the character that needs to be printed n number of times is
-		# #in the second spot in the tuple
-		# char=t[1]
+		#the character that needs to be printed n number of times is
+		#in the second spot in the variable
+		char=t[1]
 		
-		# #write the desired number of chars
-		# res.append(n*char)	
+		#write the desired number of chars
+		res.append(n*char)	
 			
-	# #return a string that is equal to the list 
-	# #with no seperation between the elements.  	
-	# return ''.join(res)
+	#return a string that is equal to the list 
+	#with no seperation between the elements.  	
+	return ''.join(res)
 
 # def main(argv):
 	# if  len(sys.argv)==3:
